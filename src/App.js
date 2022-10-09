097049636021c4c9346650a98763e2db9f4abd9d
AndresPalmezanoG <palmezano20.02@hotmail.com>
2022-10-09 02:27:32 -0500

    sexto commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Pokedex from './components/Pokedex';
 import Searchbar from './components/Searchbar';
-import { getPokemons } from './api';
+import { getPokemonData, getPokemons } from './api';
 
 const {useState, useEffect} = React;
 
@@ -13,14 +13,18 @@ function App() {
   const fetchPokemons = async () => {
     try {
       const data = await getPokemons();
-      console.log(data);
+      const promises = data.results.map(async (pokemon) => {
+      return await getPokemonData(pokemon.url);
+      })
+      const results = await Promise.all(promises)
+      setPokemon(results)
     } catch (err){
       
     }
   }
 
   useEffect(() => {
-      console.log("Inside useEffect")
+      fetchPokemons();
   }, [])
 
   return (
@@ -28,7 +32,7 @@ function App() {
       <Navbar />
       <div className="App">
         <Searchbar />
-        <Pokedex />
+        <Pokedex pokemons={pokemons}/>
       </div>
     </div>
   );
