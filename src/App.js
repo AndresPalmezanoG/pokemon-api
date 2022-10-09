c8aa985e2b4c7a257125182c09910959d4b5198e
AndresPalmezanoG <palmezano20.02@hotmail.com>
2022-10-09 01:49:45 -0500

    quinto commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Pokedex from './components/Pokedex';
 import Searchbar from './components/Searchbar';
+import { getPokemons } from './api';
+
+const {useState, useEffect} = React;
 
 function App() {
+  const [pokemons, setPokemon] = useState([]);
+
+  const fetchPokemons = async () => {
+    try {
+      const data = await getPokemons();
+      console.log(data);
+    } catch (err){
+      
+    }
+  }
+
+  useEffect(() => {
+      console.log("Inside useEffect")
+  }, [])
+
   return (
     <div>
       <Navbar />
       <div className="App">
         <Searchbar />
+        <Pokedex />
       </div>
     </div>
   );
